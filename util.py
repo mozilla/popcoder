3ae8c0711b5a04bd410dee823a30fbc1ff080627
Mike Nolan <me@nolski.rocks>
2015-07-06 15:11:22 -0700

    Added some utility functions

---
+++ util.py
@@ -0,0 +1,25 @@
+def seconds_to_timecode(sec):
+    seconds = int(sec % 60)
+    minutes = int(sec / 60)
+    hours = int(sec / 3600)
+
+    if seconds < 10:
+        strsec = '0' + str(seconds)
+    else:
+        strsec = str(seconds)
+
+    if minutes < 10:
+        strmin = '0' + str(minutes)
+    else:
+        strmin = str(minutes)
+
+    if hours < 10:
+        strhours = '0' + str(hours)
+    else:
+        strhours = str(hours)
+
+    return strhours + ':' + strmin + ':' + strsec
+
+
+def percent_to_px(val, scale):
+    return (scale / 100) * val
